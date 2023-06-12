3d046c0b28cd7648913e93a9523c26ee09cd3f32
Dylan Koss <dylanhkoss@gmail.com>
2023-06-12 16:02:54 -0500

    added favicon/logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,15 +2,19 @@
 import Link from "next/link"
 import React, { useState } from "react"
 import { LuAlignRight, LuMenu } from "react-icons/lu"
+import logo from "../../public/assets/favicon.png"
 
 export default function Navbar() {
   const [activeMenu, setActiveMenu] = useState(false)
   return (
-    <nav className="bg-gray-100 py-4 px-8 flex items-center justify-between shadow-md fixed top-0 z-20 w-full bg-opacity-60">
+    <nav className="bg-gray-100 py-4 px-8 flex items-center justify-between shadow-md fixed top-0 z-20 w-full bg-opacity-75">
       <Link href="/">
-        <h1 className="text-2xl font-semibold cursor-pointer select-none">
-          OtterDrone
-        </h1>
+        <div className="flex items-center">
+          <h1 className="text-2xl font-semibold cursor-pointer select-none">
+            OtterDrone
+          </h1>
+          <img src={logo.src} alt="otter image" className="w-8 h-8 ml-1" />
+        </div>
       </Link>
       <div onClick={() => setActiveMenu((prev) => !prev)} className="relative">
         {!activeMenu ? (
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/assets/favicon.png" />
+      </head>
       <body className={cabin.className}>
         <Navbar />
         {children}
