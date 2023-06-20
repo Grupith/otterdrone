9de19ed587f667c8827bc1a7372dcc14fde11271
Dylan Koss <dylanhkoss@gmail.com>
2023-06-19 23:09:49 -0500

    updated projects page UI

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,109 +1,99 @@
 import Link from "next/link"
 import React from "react"
+import { IoCheckmarkSharp } from "react-icons/io5"
+
+interface Card {
+  title: string
+  subtitle: string
+  price: string
+  features: string[]
+}
 
 export default function Pricing() {
+  const PricingCards: Card[] = [
+    {
+      title: "Real Estate",
+      subtitle: "Basic",
+      price: "$249",
+      features: [
+        "Interior Video Tour",
+        "Exterior Aerial Photos",
+        "Same Day Delivery",
+      ],
+    },
+    {
+      title: "Real Estate",
+      subtitle: "Pro",
+      price: "$499",
+      features: [
+        "Interior/Exterior Video Tour",
+        "Professionally Edited Photos",
+        "Instantly Post to Realtor Sites",
+        "Next Day Delivery",
+      ],
+    },
+    {
+      title: "Business Tour",
+      subtitle: "Pro",
+      price: "$699",
+      features: [
+        "Market Your Business",
+        "Attract New Employees",
+        "1-4 Minute Video",
+        "Professionally Edited",
+        "Companies, Restaurants, Golf Courses, Shops, Etc.",
+      ],
+    },
+    {
+      title: "Event",
+      subtitle: "Public/Private",
+      price: "$899",
+      features: [
+        "Capture Event Highlights",
+        "Professionally edited",
+        "1-10 Minute Video",
+        "Corporate Events, Weddings, Parades, Concerts, Etc.",
+      ],
+    },
+    {
+      title: "Custom",
+      subtitle: "Pro",
+      price: "$$$",
+      features: [
+        "Multiple Day Project?",
+        "Personal Aerial Photos/Videos?",
+        "Construction Projects?",
+      ],
+    },
+  ]
   return (
     <div className="pt-16 bg-gray-200 min-h-screen overflow-x-hidden">
-      <h1 className="text-center text-4xl mt-10 font-semibold">Our Pricing</h1>
-      <p className="text-center pt-6 text-xl mx-16">Choose your Package Type</p>
-      <section className="my-10 space-y-12 sm:flex sm:justify-between px-6">
-        {/* Real Estate Basic */}
-        <div className="border border-black/30 rounded-lg w-full m-auto px-10 bg-gradient-to-tr from-stone-200 to-stone-300 py-4 shadow-lg">
-          <h2 className="text-3xl font-medium text-center">Real Estate</h2>
-          <h3 className="text-xl text-center py-1">Basic</h3>
-          <p className="text-center text-5xl py-2 font-semibold">$249</p>
-          <ul className="list-disc list-inside text-lg py-2">
-            <li>Interior Video Tour</li>
-            <li>Exterior Aerial Photos</li>
-            <li>Same Day Delivery</li>
-          </ul>
-          <div className="flex justify-center py-4">
-            <Link href="/contact">
-              <button className="bg-blue-500 py-1 px-8 rounded-md text-white text-lg font-medium shadow-lg hover:scale-105 transition-all">
+      <h1 className="text-center text-4xl mt-10 font-semibold">
+        Choose your Package
+      </h1>
+      <p className="text-center pt-6 text-xl mx-16">
+        Quick setup! Once complete, OtterDrone will contact you shortly!
+      </p>
+      <div className="flex flex-col justify-center items-center my-4 space-y-10">
+        {PricingCards.map((card) => (
+          <div className="white border border-black/25 rounded-lg bg-white w-fit py-8 px-8 space-y-4 shadow-lg">
+            <h2 className="text-center text-4xl font-medium">{card.title}</h2>
+            <h3 className="text-center text-xl font-light">{card.subtitle}</h3>
+            <p className="text-center text-5xl font-semibold">{card.price}</p>
+            {card.features.map((feature) => (
+              <div className="flex items-center text-xl">
+                <IoCheckmarkSharp />
+                <span className="ml-2">{feature}</span>
+              </div>
+            ))}
+            <div className="flex justify-center py-2">
+              <button className="text-lg text-white font-medium rounded-md bg-gradient-to-tr from-blue-500 to-blue-600 py-1 px-12 hover:scale-105 transition-all">
                 Start Package
               </button>
-            </Link>
+            </div>
           </div>
-        </div>
-        {/* Real Estate Pro */}
-        <div className="border border-black/30 rounded-lg w-full m-auto px-10 bg-white py-4 shadow-lg bg-gradient-to-tr from-stone-200 to-stone-300">
-          <h2 className="text-3xl font-medium text-center">Real Estate</h2>
-          <h3 className="text-xl text-center font-semibold py-1">Pro</h3>
-          <p className="text-center text-5xl py-2 font-semibold">$499</p>
-          <ul className="list-disc list-inside text-lg py-2">
-            <li>Interior/Exterior Video Tour</li>
-            <li>Professionally Edited Photos</li>
-            <li>Instantly Post to Realtor Sites</li>
-            <li>Next Day Delivery</li>
-          </ul>
-          <div className="flex justify-center py-4">
-            <Link href="/contact">
-              <button className="bg-blue-500 py-1 px-8 rounded-md text-white text-lg font-medium shadow-lg hover:scale-105 transition-all">
-                Start Package
-              </button>
-            </Link>
-          </div>
-        </div>
-        {/* Business Tour */}
-        <div className="border border-black/30 rounded-lg w-full m-auto px-10 bg-white py-4 shadow-lg bg-gradient-to-tr from-indigo-100 to-indigo-200">
-          <h2 className="text-3xl font-medium text-center">Business Tour</h2>
-          <h3 className="text-xl text-center font-semibold py-1">Pro</h3>
-          <p className="text-center text-5xl py-2 font-semibold">$699</p>
-          <ul className="list-disc list-inside text-lg py-2">
-            <li>Market Your Business</li>
-            <li>Attract New Employees</li>
-            <li>1-4 Minute Video</li>
-            <li>Professionally Edited</li>
-            <li>Companies, Resturants, Golf Courses, Shops, Etc.</li>
-          </ul>
-          <div className="flex justify-center py-4">
-            <Link href="/contact">
-              <button className="bg-blue-500 py-1 px-8 rounded-md text-white text-lg font-medium shadow-lg hover:scale-105 transition-all">
-                Start Package
-              </button>
-            </Link>
-          </div>
-        </div>
-        {/* Event */}
-        <div className="border border-black/30 rounded-lg w-full m-auto px-10 bg-white py-4 shadow-lg bg-gradient-to-tr from-indigo-100 to-indigo-200">
-          <h2 className="text-3xl font-medium text-center">Event</h2>
-          <h3 className="text-lg text-center font-semibold py-1">
-            Public/Private
-          </h3>
-          <p className="text-center text-5xl py-2 font-semibold">$899</p>
-          <ul className="list-disc list-inside text-lg py-2">
-            <li>Capture Event Highlights</li>
-            <li>Professionally edited</li>
-            <li>1-10 Minute Video</li>
-            <li>Coorperate Events, Weddings, Parades, Concerts, Etc.</li>
-          </ul>
-          <div className="flex justify-center py-4">
-            <Link href="/contact">
-              <button className="bg-blue-500 py-1 px-8 rounded-md text-white text-lg font-medium shadow-lg hover:scale-105 transition-all">
-                Start Package
-              </button>
-            </Link>
-          </div>
-        </div>
-        {/* Custom */}
-        <div className="border border-black/30 rounded-lg w-full m-auto px-10 bg-white py-4 shadow-lg bg-gradient-to-tr from-rose-200 to-rose-300">
-          <h2 className="text-3xl font-medium text-center">Custom</h2>
-          <h3 className="text-xl text-center font-semibold py-1">Pro</h3>
-          <p className="text-center text-5xl py-2 font-semibold">$$$</p>
-          <ul className="list-disc list-inside text-lg py-2">
-            <li>Multiple Day Project?</li>
-            <li>Personal Aerial Photos/Videos?</li>
-            <li>Construction Projects?</li>
-          </ul>
-          <div className="flex justify-center py-4">
-            <Link href="/contact">
-              <button className="bg-blue-500 py-1 px-8 rounded-md text-white text-lg font-medium shadow-lg hover:scale-105 transition-all">
-                Contact
-              </button>
-            </Link>
-          </div>
-        </div>
-      </section>
+        ))}
+      </div>
     </div>
   )
 }
