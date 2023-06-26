c43dd7e13b8db43ab84fabfc5be852369cad8745
Dylan Koss <dylanhkoss@gmail.com>
2023-06-26 18:01:29 -0500

    added grid UI to pricing page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import { IoCheckmarkSharp } from "react-icons/io5"
 
 interface Card {
+  id: string
   title: string
   subtitle: string
   price: string
@@ -12,6 +13,7 @@ interface Card {
 export default function Pricing() {
   const PricingCards: Card[] = [
     {
+      id: "p1",
       title: "Real Estate",
       subtitle: "Basic",
       price: "$249",
@@ -22,6 +24,7 @@ export default function Pricing() {
       ],
     },
     {
+      id: "p2",
       title: "Real Estate",
       subtitle: "Pro",
       price: "$499",
@@ -33,6 +36,7 @@ export default function Pricing() {
       ],
     },
     {
+      id: "p3",
       title: "Business Tour",
       subtitle: "Pro",
       price: "$699",
@@ -45,6 +49,7 @@ export default function Pricing() {
       ],
     },
     {
+      id: "p4",
       title: "Event",
       subtitle: "Public/Private",
       price: "$899",
@@ -56,6 +61,7 @@ export default function Pricing() {
       ],
     },
     {
+      id: "p5",
       title: "Custom",
       subtitle: "Pro",
       price: "$$$",
@@ -67,32 +73,39 @@ export default function Pricing() {
     },
   ]
   return (
-    <div className="pt-16 bg-gray-200 min-h-screen overflow-x-hidden">
+    <div className="pt-16 bg-gradient-to-r bg-gray-200 min-h-screen overflow-x-hidden">
       <h1 className="text-center text-4xl mt-10 font-semibold">
         Choose your Package
       </h1>
       <p className="text-center pt-6 text-xl mx-16">
-        Quick setup! Once complete, OtterDrone will contact you shortly!
+        Simple Pricing, No Hidden Costs!
       </p>
-      <div className="flex flex-col justify-center items-center my-4 space-y-10">
-        {PricingCards.map((card) => (
-          <div className="white border border-black/25 rounded-lg bg-white w-fit py-8 px-8 space-y-4 shadow-lg">
-            <h2 className="text-center text-4xl font-medium">{card.title}</h2>
-            <h3 className="text-center text-xl font-light">{card.subtitle}</h3>
-            <p className="text-center text-5xl font-semibold">{card.price}</p>
-            {card.features.map((feature) => (
-              <div className="flex items-center text-xl">
-                <IoCheckmarkSharp />
-                <span className="ml-2">{feature}</span>
+      <div className="flex justify-center my-10">
+        <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+          {PricingCards.map((card) => (
+            <div
+              key={card.id}
+              className="white border border-black/10 rounded-lg bg-gray-100 w-96 h-full py-8 px-8 space-y-4 shadow-lg"
+            >
+              <h2 className="text-center text-4xl font-medium">{card.title}</h2>
+              <h3 className="text-center text-xl font-light">
+                {card.subtitle}
+              </h3>
+              <p className="text-center text-5xl font-semibold">{card.price}</p>
+              {card.features.map((feature) => (
+                <div key={feature} className="flex items-center text-xl">
+                  <IoCheckmarkSharp />
+                  <span className="ml-2">{feature}</span>
+                </div>
+              ))}
+              <div className="flex justify-center py-2">
+                <button className="text-lg text-white font-light rounded-md bg-gradient-to-tr from-blue-500 to-blue-600 py-1 px-12 hover:scale-105 transition-all">
+                  Start Package
+                </button>
               </div>
-            ))}
-            <div className="flex justify-center py-2">
-              <button className="text-lg text-white font-medium rounded-md bg-gradient-to-tr from-blue-500 to-blue-600 py-1 px-12 hover:scale-105 transition-all">
-                Start Package
-              </button>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   )
